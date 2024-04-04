9aacfb0f103c3d8535e73ed92662a52e4480d1b6
adityajbasu <162044553+adityajbasu@users.noreply.github.com>
2024-04-04 20:54:54 +0530

    Update Introduction-Lists.py

--- Day1/Introduction-Lists.py
+++ Day1/Introduction-Lists.py
@@ -1,7 +1,10 @@
 apples=3.8
 oranges=5
 grapes=10
-
+a="apple"
+b="ball"
+c="cat"
+fruitstring=[a,b,c]
 fruits=[apples,oranges,grapes]
 
 print(fruits)#What will this output?
@@ -9,3 +12,4 @@ print(fruits)#What will this output?
 #print(fruits[1])#What will this output
 #print(len(fruits))#''
 #print(fruits[x:y])
+#print(fruits.index())
