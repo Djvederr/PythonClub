d38fb7753e621b2e7407069618eb8a303eebfa86
adityajbasu <162044553+adityajbasu@users.noreply.github.com>
2024-04-04 21:47:17 +0530

    Update Introduction-basicdDataTypes.py

--- Day1/Introduction-basicdDataTypes.py
+++ Day1/Introduction-basicdDataTypes.py
@@ -1,4 +1,13 @@
 apples = 3.8 #What data type is this?
 oranges = 5 #What data type is this?
-print("I have " + apples + " apples and "+ oranges + "oranges.")#Will this run?
-
+banana="Python"
+print(apples)
+print(oranges)
+print(banana)
+atype=type(apples)
+otype=type(oranges)
+btype=type(banana)
+print(atype)
+print(otype)
+print(btype)
+#what do you think the type() function does
